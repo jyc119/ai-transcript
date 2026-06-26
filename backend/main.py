4f574310483cca961913037456dc6678695d5a19
Jordan Chin <jc9627@g.rit.edu>
2026-06-26 16:23:10 +0800

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+#accepts audio file and returns a transcript
+@app.get("/tasks", response_model=List[Task])
+def get_tasks():
+    return tasks
