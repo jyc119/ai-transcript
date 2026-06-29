f00eddb33878ecf0856636f8c24a6062fc4ec9db
Jordan Chin <jc9627@g.rit.edu>
2026-06-29 17:06:36 +0800

    added transcribe function

--- backend/helper.py
+++ backend/helper.py
@@ -1,6 +1,8 @@
 import sounddevice as sd
 import wavio as wv
 import numpy as np
+from openai import OpenAI
+
 
 def main():
     freq = 44100
@@ -18,7 +20,18 @@ def main():
     wv.write("recording.wav", audio, freq, sampwidth=2)
     print("Saved recording.wav")
 
+
+def transcribe():
+    client = OpenAI()
+    audio_file = open("recording.wav", "rb")
+
+    transcription = client.audio.transcriptions.create(
+        model="gpt-4o-transcribe",
+        file=audio_file
+    )
+
+    print(transcription.text)
+
+
 if __name__ == "__main__":
     main()
-
-
